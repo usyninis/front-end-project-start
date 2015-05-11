ca1f850448577cac68fd9cddda97de17823649ce
usyninis <usyninis@gmail.com>
2015-05-12 01:33:04 +0600

    create gulp watch

--- assets/js/react/mydiv.js
+++ assets/js/react/mydiv.js
@@ -1,8 +1,9 @@
 module.exports = React.createClass({
+  displayName: "MyDiv",
   render: function(){
     return (
       <div>
-       MyDiv123
+       MyDiv123s
       </div>
     )
   }
--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,32 @@
 var gulp = require('gulp'), // сборщик
+	//gutil = require('gulp-util'), // утилиты сборщика
 	browserify = require('browserify'), // include frontend support
 	source = require('vinyl-source-stream'), // return gulp pipe
 	buffer = require('vinyl-buffer'), //  return streaming file contents
 	uglify = require('gulp-uglify'), // Минификация JS
 	less = require('gulp-less'), // less to css
 	csso = require('gulp-csso'), // Минификация CSS
-	concat = require('gulp-concat'); // Склейка файлов
+	concat = require('gulp-concat'), // Склейка файлов
+	watch = require('gulp-watch'); // gulp отслеживание
 
 var react = require('react'),
 	reactify = require('reactify');
+
+var path = {
+	CSS:   'df'
+};
 	
 gulp.task('build-styles',function() {
 
   //gulp.src()
-  return gulp.src('./assets/less/style.less')
-    .on('error', console.log) // Если есть ошибки, выводим и продолжаем
+  //console.log('build-styles run');
+  return gulp.src('./assets/less/style.less')    
+    //.on('error', gutil.log) // Если есть ошибки, выводим и продолжаем
     .pipe(less()) // less
+    .on('error', swallowError)
 	//.pipe(concat('style.css'))
 	.pipe(csso())
+	.pipe(concat('style.css'))
     .pipe(gulp.dest('./public')); // даем команду на перезагрузку css
 
 
@@ -31,7 +40,9 @@ gulp.task('transform',function() {
 	  transform: ['reactify'],
       //extensions: ['.jsx'],
 	})
+  	.on('error', swallowError)
 	.bundle()
+  	.on('error', swallowError)
 	.pipe(source('client.js')) 
 	.pipe(buffer())
 	.pipe(uglify())   
@@ -64,12 +75,36 @@ gulp.task('build-scripts',function() {
 
 });
 
-gulp.task('build', function() {
+gulp.task('watch-build', function() {
 
-  //gulp.watch('./assets/less/*.less',['build-styles']);
-  //gulp.watch('./assets/js/*.js',['build-scripts']);
-  gulp.run('transform');
+  gulp.start('build-styles');
+
+  watch(['./assets/less/**/*.less'], function() {
+
+    gulp.start('build-styles');
+
+  });
+
+  gulp.start('transform');
+
+  watch(['./assets/js/**/*.js'], function() {
+
+    //gulp.start('build-scripts');
+    gulp.start('transform');
+
+  });
+  
+   //gulp.watch('./assets/js/*.js',['build-scripts']);
+  //gulp.run('transform');
   //gulp.run('build-scripts');
 
 });
 
+
+function swallowError (error) {
+
+    //If you want details of the error in the console
+    console.log(error.toString());
+
+    this.emit('end');
+}
